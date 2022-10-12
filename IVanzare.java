1e22d662d5204edb2ee654e5a103a1045298e102
bigsosa337 <115635392+bigsosa337@users.noreply.github.com>
2022-10-12 20:14:26 +0300

    Add files via upload

---
+++ IVanzare.java
@@ -0,0 +1,7 @@
+package ro.ase.seminar1.exemplu.curs4asdasd;
+
+public interface IVanzare {
+
+    float calculPretVanzare();
+
+}
---
+++ asd.java
@@ -0,0 +1,4 @@
+package ro.ase.seminar1.exemplu.curs4asdasd;
+
+public class asd {
+}
